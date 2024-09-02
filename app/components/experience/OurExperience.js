430cc02c8ae98db94e4cd2ec3d1a170b04e5a793
latz09 <85132328+latz09@users.noreply.github.com>
2024-09-02 16:00:27 -0500

    car

--- app/components/experience/OurExperience.js
+++ app/components/experience/OurExperience.js
@@ -5,6 +5,7 @@ import { FETCH_CAROUSEL_IMAGES as query } from '@/data/queries/components/FETCH_
 import Carousel from '../lib/Carousel';
 import ExperienceCarousel from '../lib/ExperienceCarousel';
 import { Heading } from '../utils/Typography';
+import { FaArrowRight } from 'react-icons/fa';
 
 const OurExperience = async () => {
 	const data = await fetchContent(query);
@@ -13,6 +14,12 @@ const OurExperience = async () => {
 		<div className="grid place-items-center gap-12 ">
 			<Heading text="Our Experience" color="text-dark" />
 			<ExperienceCarousel images={data.images} />
+			<div className='grid place-items-center'>
+				<div className='mt-4 cursor-pointer py-2 px-6 bg-primary  text-light rounded-full font-bold lg:text-lg '>
+					<span className=''>Get Started Now</span>
+					<FaArrowRight className='inline-block ml-2' />
+				</div>
+			</div>
 		
 		</div>
 	);
